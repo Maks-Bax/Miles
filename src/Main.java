6fee73b896334fd7e186593672ab0d46c32814e2
Maks-Bax <rn4aey@yandex.ru>
2022-05-11 11:07:48 +0300

    InitialCommit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9000;
+        int dose = 20;
+        int bonus = ticket / dose;
+        System.out.println("Вам начислено" + " " + bonus + " " + "милей.");
+    }
+}
